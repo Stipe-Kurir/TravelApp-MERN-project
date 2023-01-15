334d5a60d007ac8f9851a399b4c6a0a7ceed4f40
Stipe-Kurir <skurir00@fesb.hr>
2023-01-15 21:33:48 +0100

    Using React MapBox, Creating MapBox Marker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,41 @@
+import React,{useState} from 'react';
+import ReactMapGL,{Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import {Room} from "@material-ui/icons";
+
 
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude:46,
+    longitude: 17,
+    zoom: 4,
+  });
+
   return (
-    <div className="App">
-     Hello
-    </div>
-  );
+  <div>
+  <ReactMapGL
+    initialViewState={viewport}
+    style={{width: '100vw', height: '100vh'}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    onViewportChange={nextViewport=>setViewport(nextViewport)}
+  >
+    <Marker latitude={48.858093} 
+    longitude={2.294694}
+    offsetLeft={-20}
+    offsetRight={-10}
+    >
+      <Room style={{fontSize:viewport.zoom*10, color:"slateblue"}} />
+    </Marker>
+    </ReactMapGL>
+  </div>
+  )
 }
 
 export default App;
+
+
+
+
+
